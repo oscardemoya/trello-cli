fd16eff1b51ed69396294693a214574e27f2fead
Oscar De Moya <oscar.demoya@koombea.com>
2019-05-23 11:52:32 -0500

    Displaying labels and memebers in a single line in card details

--- api/card.sh
+++ api/card.sh
@@ -2,52 +2,37 @@
 
 card() {
     SELECTED_CARD_NUMBER=$1
-    
     printf $FORMAT_WHITE "Fetching trello card [#$SELECTED_CARD_NUMBER]..."
-    
     URL="$BASE_PATH/boards/$BOARD_SHORT_LINK/cards/$SELECTED_CARD_NUMBER?fields=all&members=true&member_fields=all&$AUTH_PARAMS"
-
     CARD=$(curl -s $URL | jq -r '. | @base64')
     if [ ! -z "$CARD" -a "$CARD" != " " ]; then
         _jq() {
             echo $CARD | base64 --decode | jq -r ${1}
         }
+        _jqjoined() {
+            MODEL=${1}
+            FIELD=${2}
+            echo $CARD | base64 --decode | jq -r $MODEL' | map('$FIELD') | join(", ")'
+        }
         printf $FORMAT_YELLOW "📝 Card [#$SELECTED_CARD_NUMBER]"
-        echo "ID: $(_jq '.id')"
+        SELECTED_CARD_ID=$(_jq '.id')
+        echo "ID: $SELECTED_CARD_ID"
         echo "URL: $(_jq '.shortUrl')"
         echo "NAME: $(_jq '.name')"
         echo "DESC: $(_jq '.desc')"
-        echo "LABELS:"
-        LABELS=$(_jq '.labels')
-        for LABEL in $(echo "${LABELS}" | jq -r '.[] | @base64'); do
-            _jql() {
-                echo ${LABEL} | base64 --decode | jq -r ${1}
-            }
-           echo "- $(_jql '.name')"
-        done
-        echo "MEMBERS:"
-        MEMBERS=$(_jq '.members')
-        for MEMBER in $(echo "${MEMBERS}" | jq -r '.[] | @base64'); do
-            _jqm() {
-                echo ${MEMBER} | base64 --decode | jq -r ${1}
-            }
-           echo "- $(_jqm '.fullName')"
-        done
+        echo "LABELS: $(_jqjoined '.labels' '.name')"
+        echo "MEMBERS: $(_jqjoined '.members' '.fullName')"
         echo "CHECKLISTS:"
-        CHECKLIST_IDS=$(_jq '.idChecklists')
-        for CHECKLIST_ID in $(echo "${CHECKLIST_IDS}" | jq -r '.[]'); do
-            request_checklist $CHECKLIST_ID
-        done        
-        
+        request_checklists $SELECTED_CARD_ID
     else
     	printf $FORMAT_RED "Card not found."
     fi
 }
 
-request_checklist() {
-    CHECKLIST_ID=$1
-    URL="$BASE_PATH/checklists/$CHECKLIST_ID?fields=all&$AUTH_PARAMS"
-    RESULTS=$(curl -s $URL | jq -r '. | @base64')
+request_checklists() {
+    SELECTED_CARD_ID=$1
+    URL="$BASE_PATH/cards/$SELECTED_CARD_ID/checklists?fields=all&$AUTH_PARAMS"
+    RESULTS=$(curl -s $URL | jq -r '.[] | @base64')
     for ITEM in $RESULTS; do
         _jqc() {
             echo ${ITEM} | base64 --decode | jq -r ${1}
@@ -68,11 +53,8 @@ request_checklist() {
 
 card_id() {
     SELECTED_CARD_NUMBER=$1
-    
     printf $FORMAT_WHITE "Fetching id for trello card [#$SELECTED_CARD_NUMBER]..."
-    
     URL="$BASE_PATH/boards/$BOARD_SHORT_LINK/cards/$SELECTED_CARD_NUMBER?fields=all&members=true&member_fields=all&$AUTH_PARAMS"
-
     CARD=$(curl -s $URL | jq -r '. | @base64')
     if [ ! -z "$CARD" -a "$CARD" != " " ]; then
         _jq() {
