77ea9ed2531e53c338e45c300beb7102687737c2
Oscar De Moya <oscar.demoya@koombea.com>
2019-05-24 17:05:49 -0500

    Updated doing option

--- api/doing.sh
+++ api/doing.sh
@@ -4,10 +4,8 @@ doing() {
     DOING_CARD_NUMBER=$1
     if [ -z "$DOING_CARD_NUMBER" ]; then
         doing_card $DOING_CARD_NUMBER
-    elif [ -z "$DOING_CARD_NUMBER" ]; then
-        list_cards "Doing"
     else
-        printf $FORMAT_RED "Invalid parameter '$TODO_COMMAND' for 'doing'."
+        list_cards "Doing"
     fi
 }
 
