35861345a765c9a18cbcc66abb08d924fbcaeaa7
Oscar De Moya <oscar.demoya@koombea.com>
2019-05-24 17:07:33 -0500

    Updated doing option

--- api/doing.sh
+++ api/doing.sh
@@ -2,7 +2,7 @@
 
 doing() {
     DOING_CARD_NUMBER=$1
-    if [ -z "$DOING_CARD_NUMBER" ]; then
+    if [ ! -z "$DOING_CARD_NUMBER" ]; then
         doing_card $DOING_CARD_NUMBER
     else
         list_cards "Doing"
